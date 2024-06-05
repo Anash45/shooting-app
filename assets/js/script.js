829a2b92780e14adf06cd2ccff35cc45f446fe86
Syed Anas Bukhari <anas14529@gmail.com>
2024-06-06 02:06:26 +0500

    Test 3

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function handleCredentialResponse(response) {
+    const data = jwt_decode(response.credential);
+    console.log(data);
+    // fetch('login.php', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         google_id: data.sub,
+    //         name: data.name,
+    //         email: data.email,
+    //         profile_picture: data.picture
+    //     })
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         if (data.success) {
+    //             // Handle successful login
+    //             console.log('Login successful');
+    //         } else {
+    //             // Handle login failure
+    //             console.log('Login failed');
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+}
+
+document.getElementById('g_id_onload').addEventListener('click', () => {
+    google.accounts.id.prompt();
+});
